86f384253b37c30afe1106d47ad6f062d8ed9d9b
Zzzeeke <zekecagas@gmail.com>
2024-06-20 13:27:36 +0800

    updated the routes to point the correct controllers

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/skills', function () {
 Route::get('/hobby', function () {
     return view('components/hobbies');
 });
+
+Route::get('/about-me', [AboutMeController::class, 'index'])->name('about-me');
+
+Route::get('/skills', [SkillsController::class, 'index'])->name('skills');
+
+Route::get('/hobby', [HobbiesController::class, 'index'])->name('hobbies');
